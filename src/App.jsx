186886b92c9626dbeca056c0303ee4dc769c253e
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-12-05 07:32:43 -0500

    Update - Solid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+import solidLogo from './Assets/Solid.svg'
+import viteLogo from '../Public/Vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = createSignal(0)
-
   return (
     <>
       <div>
